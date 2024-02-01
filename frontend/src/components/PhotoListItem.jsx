98dc4fc81d6bd56ee310c852af6767f71e134aa7
David Carmichael <carmichael.db88@gmail.com>
2024-02-01 22:07:13 +0000

    Add: PhotoListItem component to PhotoList, Edit: remove PhotoListItem and mock data from App.jsx, Add: PhotoList compontent to App.jsx

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,11 +5,11 @@ import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
 
-  const {location: {city, country}, imageSource, profile, username} = props.photo;
+  const {location: {city, country}, urls: {full, regular}, user: {username, name, profile}} = props.photo;
   return (
     <div className="photo-list__item">
       <PhotoFavButton />
-      <img className="photo-list__image" src={imageSource} />
+      <img className="photo-list__image" src={regular} />
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={profile} />
         <div className="photo-list__user-info">
