104c3e464f2856f6c5295d19aeda475bb61189bf
David Carmichael <carmichael.db88@gmail.com>
2024-02-03 23:28:51 +0000

    Add: Display photo and similar photos in modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -2,14 +2,42 @@ import React from 'react';
 
 import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
+import PhotoList from 'components/PhotoList';
 
 const PhotoDetailsModal = ({closeModal, photo}) => {
-  console.log(photo);
+  const {
+    id,
+    location: { city, country },
+    urls: { full },
+    user: { username, profile },
+  } = photo;
+  const similarPhotosArray = Object.values(photo.similarPhotos);
+
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={closeModal}>
-        <img src={closeSymbol} alt="close symbol" />
-      </button>
+      <div className="photo-details-modal__images">
+        <button
+          className="photo-details-modal__close-button"
+          onClick={closeModal}
+        >
+          <img src={closeSymbol} alt="close symbol" />
+        </button>
+        <img className="photo-details-modal__image" src={full} />
+        <div className="photo-details-modal__photographer-details">
+          <img
+            className="photo-details-modal__photographer-profile"
+            src={profile}
+          />
+          <div className="photo-details-modal__photographer-info">
+            {username}
+            <div className="photo-details-modal__photographer-location">
+              {city}, {country}
+            </div>
+          </div>
+        </div>
+        <div className="photo-details-modal__header">Similar Photos</div>
+        <PhotoList photos={similarPhotosArray} />
+      </div>
     </div>
   );
 };
